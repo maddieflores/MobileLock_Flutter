62011a1be2689c652983d0b92227a6d1aa1a7e79
Kihomy <kihomyvp@gmail.com>
2026-06-14 15:51:42 -0400

    Merge: se resuelven conflictos en pubspec y plugins macos

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import file_selector_macos
 import flutter_secure_storage_darwin
 import mobile_scanner
@@ -13,10 +12,5 @@ import mobile_scanner
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-=======
-import mobile_scanner
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> a90bd027bd6c2596d4a9820c3b98de1bdcd53352
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
